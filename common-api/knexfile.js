0b85c131f17b327ad386ed9aa06321e087e0f9b9
Dmytro Kozynets <Admin@MacBook-Pro-Dmytro.local>
2018-03-19 00:48:42 +0200

    db

--- common-api/knexfile.js
+++ common-api/knexfile.js
@@ -1,13 +1,14 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: {
-      database: 'pean',
-      user:     'fairray',
-      password: '123456789'
+      host: process.env.DB_HOST,
+      database: process.env.DB_USER,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
     },
     pool: {
       min: 2,
