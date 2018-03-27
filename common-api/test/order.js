0e2b0481867eddf68e52f1fb68e3a4c3f7b4b4bd
Dmytro Kozynets <Admin@MacBook-Pro-Dmytro.local>
2018-03-27 22:16:50 +0300

    eslint ignore

--- common-api/test/order.js
+++ common-api/test/order.js
@@ -1,32 +1,32 @@
-//During the test the env variable is set to test
+// During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 
 const Orders = require('../modules/orders/model');
 
-//Require the dev-dependencies
+// Require the dev-dependencies
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
+
 const should = chai.should();
 
 chai.use(chaiHttp);
-//Our parent block
+// Our parent block
 describe('Orders', () => {
-
-  beforeEach((done) => { //Before each test we empty the database
+  beforeEach((done) => { // Before each test we empty the database
     Orders
       .fetchAll()
-      .then(data => {
-        let promises = [];
+      .then((data) => {
+        const promises = [];
         data.forEach(item => promises.push(item.destroy()));
         return Promise.all(promises);
       })
-      .then(data => {
+      .then((data) => {
         done();
       })
-      .catch(err => {
+      .catch((err) => {
         console.log('err', err);
-    })
+      });
   });
   /*
     * Test the /GET route
@@ -43,5 +43,4 @@ describe('Orders', () => {
         });
     });
   });
-
 });
